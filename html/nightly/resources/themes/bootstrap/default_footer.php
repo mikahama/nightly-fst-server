720eb0a54286235bea27ab1c47c585eb0f7eaff3
Mika Hämäläinen <mika.hamalainen@helsinki.fi>
2020-02-10 16:39:38 +0200

    nightly html

---
+++ html/nightly/resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,6 @@
+
+<hr>
+
+<div class="footer">
+    The directory listing is provided by <a href="http://www.directorylister.com">Directory Lister</a>
+</div>
---
+++ html/nightly/resources/themes/bootstrap/default_header.php
@@ -0,0 +1,7 @@
+<div>
+  <h1>Wolf NLP nightly language resources</h1>
+  <p>This page lists the nightly builds for multiple languages. We provide free FST-based morphological transducers for morphological analysis and generation and CG based disambiguators. The easiest way to use the resources for NLP is by using <a href="https://github.com/mikahama/uralicNLP">UralicNLP</a></p>
+
+<h2>Supported languages</h2>
+
+</div>
