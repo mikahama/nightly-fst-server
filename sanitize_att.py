7c4769e8a29b370cb47feb4819cebfaf911e4520
Mika Hämäläinen <mika@rootroo.com>
2021-08-24 15:34:49 +0300

    Update sanitize_att.py

--- sanitize_att.py
+++ sanitize_att.py
@@ -1,1 +1,18 @@
+import sys
+from mikatools import *
+
+file = sys.argv[1]
+att = open_read(file).read()
+att = att.replace("	 	 	0.000000", "	@SPACE@	@SPACE@	0.000000")
+att = att.replace("	?	?	0.000000", "	%?	%?	0.000000")
+att = att.replace("	0	0	0.000000", "	%0	%0	0.000000")
+att = att.replace("	%	%	0.000000", "	%%	%%	0.000000")
+att = att.replace("	\"	\"	0.000000", "	%	%	0.000000")
+att = att.replace("	:	:	0.000000", "	@COLON@	@COLON@	0.000000")
+att = att.replace("					0.000000", "	@TAB@	@TAB@	0.000000")
+att = att.replace("	 	", "	@SPACE@	")
+att = att.replace("	ε	", "	@0@	")
+out = open_write(file)
+out.write(att)
+out.close()
 
